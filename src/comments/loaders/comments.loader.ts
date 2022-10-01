6fa53b6719e5b966cdee916857c14cfbc3719723
Artyom Tarasov <tarasovar457@gmail.com>
2022-10-01 10:16:52 +0600

    refactor: remove unnecessary separation of function

--- src/comments/loaders/comments.loader.ts
+++ src/comments/loaders/comments.loader.ts
@@ -7,22 +7,16 @@ import { CommentsService } from './../services/comments.service';
 @Injectable({ scope: Scope.REQUEST })
 export class CommentsLoader {
   constructor(private readonly commentsService: CommentsService) {}
-  paginationArgs: PaginationArgs;
 
-  public getPaginatedCommentsByAssetIdsLoader = new DataLoader(
-    async (assetIds: readonly string[]) => {
+  public getPaginatedCommentsForAsset(paginationArgs: PaginationArgs) {
+    return new DataLoader(async (assetIds: readonly string[]) => {
       const paginatedComments =
         await this.commentsService.getCommentsByAssetIds(
           assetIds as string[],
-          this.paginationArgs,
+          paginationArgs,
         );
 
       return assetIds.map((assetId) => paginatedComments[assetId] ?? []);
-    },
-  );
-
-  public getPaginatedCommentsForAsset(paginationArgs: PaginationArgs) {
-    this.paginationArgs = paginationArgs;
-    return this.getPaginatedCommentsByAssetIdsLoader;
+    });
   }
 }
